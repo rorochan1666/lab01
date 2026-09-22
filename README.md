566f2a73d6dc34a1dbe8f7bf2b365f13cac6e832
rorochan1666 <s2657252@student.ctihe.edu.hk>
2026-09-22 16:48:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lab_68662FBB : Print a big A
---
+++ src/lab_68662FBB.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void printscreen()
+{
+    // Please write your code here
+}
+
+// DO NOT change the code below
+#ifndef ___TEST___
+int main()
+{
+    printscreen();
+    return(0);
+}
+#endif
